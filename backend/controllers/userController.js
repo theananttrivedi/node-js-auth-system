7ccd7225a5712a22b8b10011a5ccba8fee0dd4c6
ananttrivedi0709 <theananttrivedi@gmail.com>
2022-04-30 13:53:17 +0530

    Added Status Codes and Verification function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,8 +14,49 @@ const generateJwtToken =  require('../utils/generateJwtToken');
 
 const userMiddleware =  require('../middleware/userMiddleware');
 
+//extract messages as constant
+//http status codes
+//extract into functions
 
 
+//Removes password and createdAt fields from the user object
+const configureUserObjectResponse = (user) => {
+    let userObj = { ...user }
+    delete userObj.password;
+    delete userObj.createdAt;
+    return userObj;
+}
+
+
+//Checks if the request body is complete or not and return a list of error messages
+const checkRequestForRequiredFields = (fields) => {
+    let messages = [];
+    Object.keys(fields).map((field) => {
+        if (!fields[field]) {
+            messages.push(`Field: '${Object.values({field})[0]}' is required!`)
+        }
+    });
+    return messages;
+}
+
+
+
+const validatePasswordField = (password, fields) => {
+    let messages = [];
+    if (password.length < 8) {
+        messages.push("PasswordFieldError: Password is very small. Atleast 8 characters are required!")
+    }
+
+    if (Object.keys(fields).length > 0) {
+        Object.keys(fields).map((field) => {
+            if (password.toLowerCase().search(fields[field].toLowerCase()) !== -1) {
+                messages.push(
+                    `PasswordFieldError: Password shouldn't include your '${Object.values({ field })[0]}'!`)
+            }
+        });
+    }
+    return messages;
+}
 
 
 
@@ -23,59 +64,31 @@ const userMiddleware =  require('../middleware/userMiddleware');
 const home = async (req, res) => {
     let messages = [];
     if (req.user) {
-        user = { ...req.user };
-        delete user.password;
-        delete user.createdAt;
+        user = configureUserObjectResponse(req.user);
         messages.push('User Details found Successfully!');
-        data = {
-            user: user
-        };
-        return res.json(responseFormatter(true, data, messages));
+        return res.status(200).json(responseFormatter(true, {user}, messages));
     }
-    messages.push('Unable to fetch user details!')
-    return res.json(responseFormatter(false, {}, messages))
+    messages.push('Unable to fetch user details!');
+    return res.status(404).json(responseFormatter(false, {}, messages));
 }
+
+
+
+
 
 const register = async (req, res) => {
     let user, data;
     const { name, username, password } = req.body;
     let messages = [];
 
-
-    //Part 1 Sufficient Data Validation
-
-    if (!name) {
-        messages.push('Name is required!')
-    }
-
-    if (!username) {
-        messages.push('Username is required!')
-    }
-
-    if (!password) {
-        messages.push('Password is required!')
-    }
-
-
+    messages = checkRequestForRequiredFields({name, username, password})
     //If there is an error (messages.length>0) then send error response
     if (messages.length > 0) {
         return res.status(400).json(responseFormatter(false, {}, messages))
     }
 
-    //Now if data has been sent, then follow this course
-
-    if (password.length < 8) {
-        messages.push('Password is very small. Atleast 8 characters are required!')
-    }
-
-    if (password.toLowerCase().search(name.toLowerCase()) !== -1) {
-        messages.push('Password shouldn\'t include your name')
-    }
-
-    if (password.toLowerCase().search(username.toLowerCase()) !== -1) {
-        messages.push('Password shouldn\'t include your username')
-    }
-
+    //Now if data is complete follow password validation
+    messages = validatePasswordField(password, {name, username})
     if (messages.length > 0) {
         return res.status(400).json(responseFormatter(false, {}, messages))
     }
@@ -83,7 +96,6 @@ const register = async (req, res) => {
 
 
     //Part 2 Querying Database
-
     try {
         user = await prisma.user.create({
             data: {
@@ -96,24 +108,16 @@ const register = async (req, res) => {
         if (e instanceof Prisma.PrismaClientKnownRequestError) {
             if (e.code === 'P2002') {
                 messages.push('This Username has already been taken, Please choose another one!')
-                return res.json(responseFormatter(false, {}, messages))
+                return res.status(404).json(responseFormatter(false, {}, messages))
             }
         }
         messages.push(e.toString())
-        return res.json(responseFormatter(false, {}, messages))
+        return res.status(404).json(responseFormatter(false, {}, messages))
     }
 
-    user = { ...user }
-    delete user.password;
-    delete user.createdAt;
-
-
+    user = configureUserObjectResponse(user);
     messages.push('User Registered Successfully!')
-    data = {
-        user: user,
-        token: generateJwtToken(user.username, user.id)
-    }
-    return res.json(responseFormatter(true, data, messages));
+    return res.status(201).json(responseFormatter(true, {user, token: generateJwtToken(user.username, user.id)}, messages));
 }
 
 
@@ -130,16 +134,7 @@ const login = async (req, res) => {
     let messages = [];
 
 
-    if (!username) {
-        messages.push('Username is required!')
-    }
-
-    if (!password) {
-        messages.push('Password is required!')
-    }
-
-
-    //If there is an error (messages.length>0) then send error response
+    messages = checkRequestForRequiredFields({ username, password });
     if (messages.length > 0) {
         return res.status(400).json(responseFormatter(false, {}, messages))
     }
@@ -154,7 +149,7 @@ const login = async (req, res) => {
         });
     } catch (e) {
         messages.push(e.toString());
-        return res.json(responseFormatter(false, {}, messages));
+        return res.status(404).json(responseFormatter(false, {}, messages));
     }
 
 
@@ -162,29 +157,19 @@ const login = async (req, res) => {
     //check if user exists
     if (!user) {
         messages.push('Either Username or Password is Wrong! #not exists')
-        return res.json(responseFormatter(false, {}, messages))
+        return res.status(404).json(responseFormatter(false, {}, messages))
     }
+
 
 
     if (user.username === username && await bcryptjs.compare(password, user.password)) {
-        user = { ...user }
-        delete user.password;
-        delete user.createdAt;
-
-
-        messages.push('User Registered Successfully!');
-        data = {
-            user: user,
-            token: generateJwtToken(user.username, user.id)
-        }
-
-        return res.json(responseFormatter(true, data, messages));
+        user = configureUserObjectResponse(user);
+        messages.push('User Logged-in Successfully!');
+        return res.status(200).json(responseFormatter(true, {user, token: generateJwtToken(user.username, user.id)}, messages));
     } else {
         messages.push('Either Username or Password is Wrong! #validation');
-        return res.json(responseFormatter(false, {}, messages));
+        return res.status(404).json(responseFormatter(false, {}, messages));
     }
-         
-
 }
 
 
@@ -197,9 +182,7 @@ const login = async (req, res) => {
 const updateprofile = async (req, res) => {
     if (req.user) {
         const { name, username, password } = req.body;
-        let data = {};
-        let user;
-        let messages = [];
+        let data = {}, user, messages = [];
         if (name) {
             data['name'] = name; 
         }
@@ -209,30 +192,22 @@ const updateprofile = async (req, res) => {
         }
 
         if (password) {
-            if (password.length < 8) {
-                messages.push('Password is very small. Atleast 8 characters are required!')
+            fields = {}
+            if (name && !username) {
+                messages = validatePasswordField(password, { name })
             }
-        
-            if (name && password.toLowerCase().search(name.toLowerCase()) !== -1) {
-                messages.push('Password shouldn\'t include your name')
+            if (username && !name) {
+                messages = validatePasswordField(password, { username })
             }
-
-            if (!name && password.toLowerCase().search(req.user.name.toLowerCase()) !== -1) {
-                messages.push('Password shouldn\'t include your name')
+            if (username && name) {
+                messages = validatePasswordField(password, { name, username })
             }
-        
-            if (username && password.toLowerCase().search(username.toLowerCase()) !== -1) {
-                messages.push('Password shouldn\'t include your username')
+            if (!username && !name) {
+                messages = validatePasswordField(password, {})
             }
-
-            if (!username && password.toLowerCase().search(req.user.username.toLowerCase()) !== -1) {
-                messages.push('Password shouldn\'t include your username')
-            }
-        
             if (messages.length > 0) {
                 return res.status(400).json(responseFormatter(false, {}, messages))
             }
-            
             data['password'] = bcryptjs.hashSync(password); 
         }
 
@@ -243,27 +218,23 @@ const updateprofile = async (req, res) => {
                 },
                 data: data
             });
-            user = { ...user }
-            delete user.password;
-            delete user.createdAt;
+            user = configureUserObjectResponse(user);
             messages.push('Profile has been updated successfully!');
-            return res.json(responseFormatter(false, user, messages));
+            return res.status(201).json(responseFormatter(false, user, messages));
             
         } catch (e) {
             if (e instanceof Prisma.PrismaClientKnownRequestError) {
                 if (e.code === 'P2002') {
                     messages.push('This Username has already been taken, Please choose another one!');
-                    return res.json(responseFormatter(false, {}, messages));
+                    return res.status(404).json(responseFormatter(false, {}, messages));
                 }
             }
             messages.push(e.toString())
-            return res.json(responseFormatter(false, {}, messages))
+            return res.status(404).json(responseFormatter(false, {}, messages))
         }
-
-        
     }
     messages.push('Unable to update profile!')
-    return res.json(responseFormatter(false, {}, messages))
+    return res.status(404).json(responseFormatter(false, {}, messages))
 }
 
 
@@ -283,15 +254,12 @@ const deleteaccount = async (req, res) => {
                 id: Number(req.user.id)
             }
         });
-
-        user = { ...user }
-        delete user.password;
-        delete user.createdAt;
+        user = configureUserObjectResponse(user);
         messages.push('Account Deleted Successfully!');
-        return res.json(responseFormatter(true, user, messages));
+        return res.status(201).json(responseFormatter(true, user, messages));
     }
     messages.push('Unable to delete account!')
-    return res.json(responseFormatter(false, {}, messages))
+    return res.status(404).json(responseFormatter(false, {}, messages))
     
 }
 
